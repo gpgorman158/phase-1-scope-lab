98cc6e99ec6faaabbbd5a8660b904c63f25b962c
Gerald Gorman <gpgorman158@gmail.com>
2022-02-02 11:35:28 -0500

    commit tree 42ec44d1c2d0bf1e8a9a411bc1443d4959b601ec

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = "gerald";
+
+console.log(customerName);
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase();
+}
+console.log(upperCaseCustomerName());
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer (){
+    leastFavoriteCustomer = 'not gerald';
+}
+//console.log(changeLeastFavoriteCustomer());
+console.log(leastFavoriteCustomer);
